99a50b18737b407bbe7754401ef85797d0e737be
justin0979 <justinm0979@gmail.com>
2020-11-10 13:21:32 -0600

    add html image responsiveness with srcSet for section-about

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -57,20 +57,18 @@ export class About extends React.Component {
                 src={nat1}
               />
               <img
-                srcSet={`${nat1sm} 300w, ${nat1} 1000w`}
-                sizes="(max-width: 900px)"
-                alt="Photo 1"
-                className="composition__photo composition__photo--p1"
-              />
-              <img
                 srcSet={`${nat2sm} 300w, ${nat2} 1000w`}
+                sizes="(max-width: 900px) 20vw, (max-width: 600px) 30vw, 300px"
                 alt="Photo 2"
                 className="composition__photo composition__photo--p2"
+                src={nat2}
               />
               <img
                 srcSet={`${nat3sm} 300w, ${nat3} 1000w`}
+                sizes="(max-width: 900px) 20vw, (max-width: 600px) 30vw, 300px"
                 alt="Photo 3"
                 className="composition__photo composition__photo--p3"
+                src={nat3}
               />
             </div>
           </div>
